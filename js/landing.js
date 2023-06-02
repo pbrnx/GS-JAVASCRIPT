e029da60610930feda082c0775bf23f1e41ffd30
pbrnx <128100284+pbrnx@users.noreply.github.com>
2023-06-02 14:43:24 -0300

    Update landing.js

---
+++ js/landing.js
@@ -0,0 +1,10 @@
+alert('Seja bem-vindo ao projeto Agrisense!');
+
+//carrousel de imagens header
+
+document.addEventListener('DOMContentLoaded', function() {
+    var imageUrls = [
+      "./images/imagem-carroussel-1.jpg",
+      "./images/imagem-carroussel-2.jpg",
+      "./images/imagem-carroussel-3.jpg"
+    ];
